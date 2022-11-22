56520d553a8f3f9433e54d78f4ff4982e02bad7b
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-11-22 19:58:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   int a = 1;
+   int b = 1;
+   int c = 0;
+   int n = 0;
+   cin >> n;
+   
+   if(n<2){
+   cout << "errore" << endl;
+   } else {
+      for(int i=0;i<n;i++){
+      cout << a << endl;
+         c = a+b;
+         a = b;
+         b = c;
+      }
+   }
    return 0;
 }
